4004fb40b2a380bd98c3d603e0f828a2bff14f5f
laurenej <laurensemma@gmail.com>
2016-04-30 14:16:45 -0400

    Update math_spec.js

--- spec/math_spec.js
+++ spec/math_spec.js
@@ -1,5 +1,13 @@
+// tell jasmine where to find our code (.. means the parent directory, of math)
+var math = require('../math');
+
 describe("A program that does basic arithmetic", function() {
 	it("can add two numbers together", function() {
-		expect(add(3,5)), toBe(8);
+		expect(math.add(3,5)).toBe(8);
 	});
+
+	it("can subtract two numbers", function() {
+		expect(math.subtract(3,5)).toBe(-2);
+	});
+
 });
